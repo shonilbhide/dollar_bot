1db4e55e60ecb5c1401cc56b5e563bb7aae48a81
sakshibasapure <sakshibasapure2@gmail.com>
2023-10-13 18:27:08 -0400

    Issue_13_Enhancement: Confirmation massge for delete option

--- code/delete.py
+++ code/delete.py
@@ -16,13 +16,27 @@ def run(message, bot):
     chat_id = message.chat.id
     delete_history_text = ""
     user_list = helper.read_json()
-    if str(chat_id) in user_list:
-        helper.write_json(deleteHistory(chat_id))
-        delete_history_text = "History has been deleted!"
-    else:
-        delete_history_text = "No records there to be deleted. Start adding your expenses to keep track of your spendings!"
-    bot.send_message(chat_id, delete_history_text)
 
+    msg = bot.send_message(chat_id, "Do you want to delete complete history? Y/N")
+    bot.register_next_step_handler(msg, delete_history, bot)
+
+
+def delete_history(message, bot):
+    chat_id = message.chat.id
+    if str(message.text) == "Y" or str(message.text) == "y":
+        if str(chat_id) in user_list:
+            helper.write_json(deleteHistory(chat_id))
+            delete_history_text = "History has been deleted!"
+        else:
+            delete_history_text = "No records there to be deleted. Start adding your expenses to keep track of your spendings!"
+        bot.send_message(chat_id, delete_history_text)
+    elif str(message.text) == "N" or str(message.text) == "n":
+            delete_history_text = "History not deleted."
+            bot.send_message(chat_id, delete_history_text)
+    else:
+            msg = bot.send_message(chat_id, "Cannot recognize the character. Enter Y/N.")
+            bot.register_next_step_handler(msg, delete_history, bot)       
+    
 
 # function to delete a record
 def deleteHistory(chat_id):
